eadc2ba9f94b7159fbf9ab6967c4eb2d9befc4c9
julioi Labrador <juliotelcel@hotmail.com>
2024-05-12 02:34:37 -0400

    uso de plantillas y carga de contenido estatico

--- routes/routes.go
+++ routes/routes.go
@@ -6,43 +6,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
 type Usuario struct {
 	Nombre string `json:"nombre"`
 	Email  string `json:"email"`
 }
 
 var Usuarios []Usuario
+*/
 
 func SetupRoutes(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "ruta inicio!")
-	})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		// Renderizar la plantilla index.html
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Title":   "Mi Aplicación",
+			"Heading": "¡aplicacion web!",
+			"Message": "Bienvenido a mi aplicación web con go.",
 		})
 	})
 
-	r.GET("/saludo/:nombre", func(c *gin.Context) {
-		nombre := c.Param("nombre")
-		c.String(http.StatusOK, "hola, %s", nombre)
-	})
-
-	r.POST("/usuarios", func(c *gin.Context) {
-		var nuevoUsuario Usuario
-		if err := c.BindJSON(&nuevoUsuario); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al decodificar el JSON"})
-			return
-		}
-
-		if nuevoUsuario.Nombre == "" || nuevoUsuario.Email == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Nombre y correo electrónico son campos requeridos"})
-			return
-		}
-
-		Usuarios = append(Usuarios, nuevoUsuario)
-
-		c.JSON(http.StatusOK, gin.H{"mensaje": "Usuario registrado", "datos": Usuarios})
-	})
+	r.Static("/static", "./static")
 }
---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+function changeMessage() {
+    const messageElement = document.getElementById("message");
+    const currentMessage = messageElement.textContent;
+    // Lista de mensajes alternativos
+    const alternateMessages = [
+    "¡Hola de nuevo!",
+    "¡Bienvenido de vuelta!",
+    "¡Qué tengas un buen día!"
+    ];
+    // Generar un mensaje aleatorio que no sea igual al mensaje actual
+    let newMessage;
+    do {
+    newMessage = alternateMessages[Math.floor(Math.random() *
+    alternateMessages.length)];
+    } while (newMessage === currentMessage);
+    // Actualizar el contenido del elemento de mensaje
+    messageElement.textContent = newMessage;
+    }
