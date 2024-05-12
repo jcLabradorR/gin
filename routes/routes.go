42076d5c6fb85b56f97bef75c563456d60d5bc01
julioi Labrador <juliotelcel@hotmail.com>
2024-05-12 00:55:01 -0400

    manejo de solicitudes http

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Usuario struct {
+	Nombre string `json:"nombre"`
+	Email  string `json:"email"`
+}
+
+var Usuarios []Usuario
+
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "ruta inicio!")
@@ -20,5 +27,22 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/saludo/:nombre", func(c *gin.Context) {
 		nombre := c.Param("nombre")
 		c.String(http.StatusOK, "hola, %s", nombre)
+	})
+
+	r.POST("/usuarios", func(c *gin.Context) {
+		var nuevoUsuario Usuario
+		if err := c.BindJSON(&nuevoUsuario); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al decodificar el JSON"})
+			return
+		}
+
+		if nuevoUsuario.Nombre == "" || nuevoUsuario.Email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Nombre y correo electrónico son campos requeridos"})
+			return
+		}
+
+		Usuarios = append(Usuarios, nuevoUsuario)
+
+		c.JSON(http.StatusOK, gin.H{"mensaje": "Usuario registrado", "datos": Usuarios})
 	})
 }
