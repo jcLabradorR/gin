2c1384407614811faa39c76ecf55c8f2a2dd5245
julioi Labrador <juliotelcel@hotmail.com>
2024-05-16 01:07:08 -0400

    adaptacion de plantilla

--- routes/routes.go
+++ routes/routes.go
@@ -2,31 +2,36 @@ package routes
 
 import (
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-/*
-type Usuario struct {
-	Nombre string `json:"nombre"`
-	Email  string `json:"email"`
-}
-
-var Usuarios []Usuario
-*/
-
 func SetupRoutes(r *gin.Engine) {
 
-	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+
+	r.LoadHTMLGlob("templates/*html")
 
 	r.GET("/", func(c *gin.Context) {
 		// Renderizar la plantilla index.html
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Title":   "Mi Aplicación",
-			"Heading": "¡aplicacion web!",
-			"Message": "Bienvenido a mi aplicación web con go.",
-		})
+		c.HTML(http.StatusOK, "index.html", nil)
+
 	})
 
-	r.Static("/static", "./static")
+	r.GET("/:page", func(c *gin.Context) {
+		page := c.Param("page")
+
+		if !strings.HasSuffix(page, ".html") {
+			page += ".html"
+		}
+
+		if _, err := os.Stat("templates/" + page); err == nil {
+			c.HTML(http.StatusOK, page, nil)
+		} else {
+			c.HTML(http.StatusNotFound, "404.html", nil)
+		}
+
+	})
 }
